de62ae694ba6c83cf0fabc1a54bcf25b8d8fb842
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-07-30 18:24:54 -0500

    update num_bivector test

--- test/num_bivector.py
+++ test/num_bivector.py
@@ -6,15 +6,16 @@ from numpoisson.numpoisson import NumPoissonGeometry
 
 npg = NumPoissonGeometry(3, 'x')
 P_so3 = {(1, 2): 'x3', (1, 3): '-x2', (2, 3): 'x1'}
-Qmesh_10_3 = np.random.rand(10**3, 3)
 
-tiempos = dict()
+num_bivector_res = dict()
+for i in [2, 3, 4, 5, 6, 7, 8]
+    Qmesh_10_3 = np.random.rand(10**i, 3)
+    tiempos = dict()
+    for k in range(25):
+        A = datetime.datetime.now()
+        npg.num_bivector(P_so3, Qmesh_10_3, pt_output=True)
+        B = datetime.datetime.now()
+        tiempos[k] = (B - A).total_seconds()
+    num_bivector_res[f'10**{i}'] = tiempos
 
-for k in range(25):
-    A = datetime.datetime.now()
-    npg.num_bivector(P_so3, Qmesh_10_3, pt_output=True)
-    B = datetime.datetime.now()
-    tiempos[k] = (B - A).total_seconds()
-
-print(f'tiempos: {tiempos}')
-print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
+print(num_bivector_res)
