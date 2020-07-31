9739ca32e7a5825f649f5118232a2718da9620fa
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-07-30 23:50:11 -0500

    update num_flaschka_ratiu_bivector test

--- test/num_flaschka_ratiu_bivector.py
+++ test/num_flaschka_ratiu_bivector.py
@@ -4,16 +4,24 @@ import numpy as np
 import statistics as stat
 from numpoisson.numpoisson import NumPoissonGeometry
 
+print('Start')
 npg = NumPoissonGeometry(4, 'x')
 functions = ['1/2*x4', '-x1**2 + x2**2 + x3**2']
-Qmesh_10_4 = np.random.rand(10**4, 4)
 
-tiempos = dict()
-for k in range(25):
-    A = datetime.datetime.now()
-    npg.num_flaschka_ratiu_bivector(functions, Qmesh_10_4, pt_output=True)
-    B = datetime.datetime.now()
-    tiempos[k] = (B - A).total_seconds()
+num_flaschka_ratiu_bivector_res = dict()
+for i in [2, 3, 4, 5, 6]:
+    print(f'step {i}')
+    Qmesh_10_4 = np.random.rand(10**i, 4)
+    tiempos = dict()
+    for k in range(25):
+        A = datetime.datetime.now()
+        npg.num_flaschka_ratiu_bivector(functions, Qmesh_10_4, pt_output=True)
+        B = datetime.datetime.now()
+        tiempos[k] = (B - A).total_seconds()
+    num_flaschka_ratiu_bivector_res[f'10**{i}'] = tiempos
 
-print(f'tiempos: {tiempos}')
-print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
+print(num_flaschka_ratiu_bivector_res)
+print('Finish')
+
+# print(f'tiempos: {tiempos}')
+# print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
