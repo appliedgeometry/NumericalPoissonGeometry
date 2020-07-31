733402e05ccd74344dc833d2d70927e083f76803
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-07-30 22:48:55 -0500

    update num_bivector test

--- test/num_bivector.py
+++ test/num_bivector.py
@@ -4,11 +4,13 @@ import numpy as np
 import statistics as stat
 from numpoisson.numpoisson import NumPoissonGeometry
 
+print('Start')
 npg = NumPoissonGeometry(3, 'x')
 P_so3 = {(1, 2): 'x3', (1, 3): '-x2', (2, 3): 'x1'}
 
 num_bivector_res = dict()
-for i in [2, 3, 4, 5, 6, 7, 8]:
+for i in [2, 3, 4, 5, 6]:
+    print(f'step {i}')
     Qmesh_10_3 = np.random.rand(10**i, 3)
     tiempos = dict()
     for k in range(25):
@@ -19,3 +21,4 @@ for i in [2, 3, 4, 5, 6, 7, 8]:
     num_bivector_res[f'10**{i}'] = tiempos
 
 print(num_bivector_res)
+print('Finish')
