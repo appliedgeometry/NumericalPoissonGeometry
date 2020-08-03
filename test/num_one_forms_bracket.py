af03fb32cd3349139d2cbe6377cbae249262d104
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-03 03:19:38 -0500

    update num_one_forms_bracket test

--- test/num_one_forms_bracket.py
+++ test/num_one_forms_bracket.py
@@ -1,4 +1,3 @@
-# Pending to be verified
 import datetime
 import time
 import numpy as np
@@ -10,15 +9,20 @@ P = {(1, 4): 1, (2, 5): 1, (3, 6): 1, (5, 6): 'x2**2'}
 alpha = {(5,): 1}
 beta = {(6,): 1}
 
-Qmesh_10_3 = np.random.rand(10**3, 6)
+num_one_forms_bracket_res = dict()
+for i in [2, 3, 4, 5, 6]:
+    print(f'step {i}')
+    Qmesh_10_3 = np.random.rand(10**i, 6)
+    tiempos = dict()
+    for k in range(25):
+        A = datetime.datetime.now()
+        npg.num_one_form_bracket(P, alpha, beta, Qmesh_10_3 , pt_output=True)
+        B = datetime.datetime.now()
+        tiempos[k] = (B - A).total_seconds()
+    num_one_forms_bracket_res[f'10**{i}'] = tiempos
 
-tiempos = dict()
+print(num_one_forms_bracket_res)
+print('Finish')
 
-for k in range(25):
-    A = datetime.datetime.now()
-    npg.num_one_form_bracket(P, alpha, beta, Qmesh_10_3 , pt_output=True)
-    B = datetime.datetime.now()
-    tiempos[k] = (B - A).total_seconds()
-
-print(f'tiempos: {tiempos}')
-print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
+# print(f'tiempos: {tiempos}')
+# print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
