ed604cd67c183e1358ee1bbdd870e0bfe4d4fa05
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-06 00:35:49 -0500

    update num_poisson_bracket test

--- test/num_poisson_bracket.py
+++ test/num_poisson_bracket.py
@@ -4,7 +4,6 @@ import numpy as np
 import statistics as stat
 from numpoisson.numpoisson import NumPoissonGeometry
 
-print('Start')
 npg = NumPoissonGeometry(6, 'x')
 P = {(1,2): 0, (1,3): 0, (1,4): 1, (1,5): 0, (1,6): 0,
      (2,3): 0, (2,4): 0, (2,5): 1, (2,6): 0,
@@ -14,18 +13,23 @@ P = {(1,2): 0, (1,3): 0, (1,4): 1, (1,5): 0, (1,6): 0,
 f, g = 'x6', 'x5'
 
 num_poisson_bracket_res = dict()
-for i in [2, 3, 4, 5, 6]:
-    print(f'step {i}')
-    Qmesh_10_3 = np.random.rand(10**i, 6)
+j = 2
+for mesh_path in ['6Qmesh_10_2.npy', '6Qmesh_10_3.npy' , '6Qmesh_10_4.npy' , '6Qmesh_10_5.npy', '6Qmesh_10_6.npy', '6Qmesh_10_7.npy']:
+    print(f'step {j}')
     tiempos = dict()
+    with open(mesh_path, 'rb') as f:
+        mesh = np.load(f)
     for k in range(25):
         A = datetime.datetime.now()
-        npg.num_poisson_bracket(P, f, g, Qmesh_10_3, pt_output=True)
+        npg.num_poisson_bracket(P, f, g, mesh, pt_output=True)
         B = datetime.datetime.now()
         tiempos[k] = (B - A).total_seconds()
-    num_poisson_bracket_res[f'10**{i}'] = tiempos
+    promedio = stat.mean(tiempos.values())
+    desviacion = stat.pstdev(tiempos.values())
+    tiempos['promedios'] = promedio
+    tiempos['desviacion'] = desviacion
+    num_poisson_bracket_res[f'10**{j}'] = tiempos
+    j = j + 1
 
 print(num_poisson_bracket_res)
 print('Finish')
-# print(f'tiempos: {tiempos}')
-# print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
