ba9f99bbecea9b8993560e9c26b119948de2463c
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-06 11:55:17 -0500

    update num_poisson_bracket test

--- test/num_poisson_bracket.py
+++ test/num_poisson_bracket.py
@@ -10,15 +10,16 @@ P = {(1,2): 0, (1,3): 0, (1,4): 1, (1,5): 0, (1,6): 0,
      (3,4): 0, (3,5): 0, (3,6): 1,
      (4,5): 0, (4,6): 0,
      (5,6): 'x2**2'}
-f, g = 'x6', 'x5'
+f = 'x6'
+g = 'x5'
 
 num_poisson_bracket_res = dict()
 j = 2
-for mesh_path in ['6Qmesh_10_2.npy', '6Qmesh_10_3.npy' , '6Qmesh_10_4.npy' , '6Qmesh_10_5.npy', '6Qmesh_10_6.npy', '6Qmesh_10_7.npy']:
+for mesh_path in ['6Qmesh_10_2.npy', '6Qmesh_10_3.npy' , '6Qmesh_10_4.npy', '6Qmesh_10_5.npy', '6Qmesh_10_6.npy', '6Qmesh_10_7.npy']:
     print(f'step {j}')
     tiempos = dict()
-    with open(mesh_path, 'rb') as f:
-        mesh = np.load(f)
+    with open(mesh_path, 'rb') as file:
+        mesh = np.load(file)
     for k in range(25):
         A = datetime.datetime.now()
         npg.num_poisson_bracket(P, f, g, mesh, pt_output=True)
