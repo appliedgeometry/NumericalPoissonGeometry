96e5f3bed10ceff26004ecaf55b262d91cd05a63
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-07 03:52:49 -0500

    update num_curl_operator test

--- test/num_curl_operator.py
+++ test/num_curl_operator.py
@@ -9,17 +9,22 @@ npg = NumPoissonGeometry(4, 'x')
 P ={(1, 3): '2*x4', (1, 4): '2*x3', (2, 3): '-2*x4', (2, 4): '2*x3', (3, 4): 'x1 - x2'}
 
 num_curl_operator_res = dict()
-for i in [2, 3, 4, 5, 6]:
-    Qmesh_10_3 = np.random.rand(10**i, 4)
+j = 2
+for mesh_path in ['4Qmesh_10_2.npy', '4Qmesh_10_3.npy', '4Qmesh_10_4.npy', '4Qmesh_10_5.npy', '4Qmesh_10_6.npy', '4Qmesh_10_7.npy']:
+    print(f'step {j}')
     tiempos = dict()
+    with open(mesh_path, 'rb') as f:
+        mesh = np.load(f)
     for k in range(25):
         A = datetime.datetime.now()
-        npg.num_curl_operator(P, 1, Qmesh_10_3, pt_output=True)
+        npg.num_curl_operator(P, 1, mesh, pt_output=True)
         B = datetime.datetime.now()
         tiempos[k] = (B - A).total_seconds()
-    num_curl_operator_res[f'10**{i}'] = tiempos
+    promedio = stat.mean(tiempos.values())
+    desviacion = stat.pstdev(tiempos.values())
+    tiempos['promedios'] = promedio
+    tiempos['desviacion'] = desviacion
+    num_curl_operator_res[f'10**{j}'] = tiempos
 
 print(num_curl_operator_res)
 print('Finish')
-# print(f'tiempos: {tiempos}')
-# print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
