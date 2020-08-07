cfca8141200bef5b4e2193caee5de5f39b0ece91
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-07 01:38:57 -0500

    update num_sharp_morphism test

--- test/num_sharp_morphism.py
+++ test/num_sharp_morphism.py
@@ -10,19 +10,23 @@ P_so3 = {(1,2): 'x3', (1,3): '-x2', (2,3): 'x1'}
 dK = {(1,): 'x1', (2,): 'x2', (3,): 'x3'}
 
 num_sharp_morphism_res = dict()
-for i in [2, 3, 4, 5, 6]:
-    print(f'step {i}')
-    Qmesh_10_3 = np.random.rand(10**i, 3)
+j = 2
+for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
+    print(f'step {j}')
     tiempos = dict()
+    with open(mesh_path, 'rb') as f:
+        mesh = np.load(f)
     for k in range(25):
         A = datetime.datetime.now()
-        npg.num_sharp_morphism(P_so3, dK, Qmesh_10_3, pt_output=True)
+        npg.num_sharp_morphism(P_so3, dK, mesh, pt_output=True)
         B = datetime.datetime.now()
         tiempos[k] = (B - A).total_seconds()
-    num_sharp_morphism_res[f'10**{i}'] = tiempos
+    promedio = stat.mean(tiempos.values())
+    desviacion = stat.pstdev(tiempos.values())
+    tiempos['promedios'] = promedio
+    tiempos['desviacion'] = desviacion
+    num_sharp_morphism_res[f'10**{j}'] = tiempos
+    j = j + 1
 
 print(num_sharp_morphism_res)
 print('Finish')
-
-# print(f'tiempos: {tiempos}')
-# print(f'Promedio = {stat.mean(tiempos.values())}\nDS = {stat.pstdev(tiempos.values())}\n')
