bc033a840f2f3801c9158282107a310b4674826a
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-07 10:50:18 -0500

    update num_flaschka_ratiu_bivector test

--- test/num_flaschka_ratiu_bivector.py
+++ test/num_flaschka_ratiu_bivector.py
@@ -9,16 +9,24 @@ npg = NumPoissonGeometry(4, 'x')
 functions = ['1/2*x4', '-x1**2 + x2**2 + x3**2']
 
 num_flaschka_ratiu_bivector_res = dict()
-for i in [2, 3, 4, 5, 6]:
-    print(f'Step: {i}')
-    Qmesh_10_4 = np.random.rand(10**i, 4)
+j = 2
+for mesh_path in ['4Qmesh_10_2.npy', '4Qmesh_10_3.npy', '4Qmesh_10_4.npy', '4Qmesh_10_5.npy', '4Qmesh_10_6.npy', '4Qmesh_10_7.npy']:
+    print(f'step {j}')
     tiempos = dict()
+    with open(mesh_path, 'rb') as f:
+        mesh = np.load(f)
     for k in range(25):
+        print(f'Step: {k}')
         A = datetime.datetime.now()
-        npg.num_flaschka_ratiu_bivector(functions, Qmesh_10_4, pt_output=True)
+        npg.num_flaschka_ratiu_bivector(functions, mesh, pt_output=True)
         B = datetime.datetime.now()
         tiempos[k] = (B - A).total_seconds()
-    num_flaschka_ratiu_bivector_res[f'10**{i}'] = tiempos
+    promedio = stat.mean(tiempos.values())
+    desviacion = stat.pstdev(tiempos.values())
+    tiempos['promedios'] = promedio
+    tiempos['desviacion'] = desviacion
+    num_flaschka_ratiu_bivector_res[f'10**{j}'] = tiempos
+    j = j + 1
 
 print(num_flaschka_ratiu_bivector_res)
 print('Finish')
