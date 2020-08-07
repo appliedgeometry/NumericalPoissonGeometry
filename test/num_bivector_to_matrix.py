939f13197d04d676845dd7c0929325a3ec263aba
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-06 22:57:07 -0500

    update num_bivector_to_matrix test

--- test/num_bivector_to_matrix.py
+++ test/num_bivector_to_matrix.py
@@ -9,16 +9,23 @@ npg = NumPoissonGeometry(3, 'x')
 P_sl2 = {(1, 2): 'x3', (1, 3): 'x2', (2, 3): 'x1'}
 
 num_bivector_to_matrix_res = dict()
-for i in [2, 3, 4, 5, 6]:
-    print(f'step {i}')
-    Qmesh_10_3 = np.random.rand(10**i, 3)
+j = 2
+for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
+    print(f'step {j}')
     tiempos = dict()
+    with open(mesh_path, 'rb') as f:
+        mesh = np.load(f)
     for k in range(25):
         A = datetime.datetime.now()
-        npg.num_bivector_to_matrix(P_sl2, Qmesh_10_3, pt_output=True)
+        npg.num_bivector_to_matrix(P_sl2, mesh, pt_output=True)
         B = datetime.datetime.now()
         tiempos[k] = (B - A).total_seconds()
-    num_bivector_to_matrix_res[f'10**{i}'] = tiempos
+    promedio = stat.mean(tiempos.values())
+    desviacion = stat.pstdev(tiempos.values())
+    tiempos['promedios'] = promedio
+    tiempos['desviacion'] = desviacion
+    num_bivector_to_matrix_res[f'10**{j}'] = tiempos
+    j = j + 1
 
 print(num_bivector_to_matrix_res)
 print('Finish')
