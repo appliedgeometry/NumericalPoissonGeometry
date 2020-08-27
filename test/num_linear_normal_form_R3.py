eefa60774079d3e748c0fe72d87503a8ce86b707
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-08-27 18:03:55 -0500

    add num_linear_normal_form_R3 test

--- test/num_linear_normal_form_R3.py
+++ test/num_linear_normal_form_R3.py
@@ -11,7 +11,7 @@ P = {(1, 2): '2*(x1 + x3)', (1, 3): '(x1 - x2)', (2, 3): '(x1 + x2 + 2*x3)'}
 num_bivector_res = dict()
 j = 2
 for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
-    print(f'step {j}')
+    print(j)
     tiempos = dict()
     with open(mesh_path, 'rb') as f:
         mesh = np.load(f)
