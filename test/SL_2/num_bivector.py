8719a320a908846d8eb795eaf968f37ae39df7cb
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-09-11 01:41:41 -0500

    update SL(2) num_bivector test

--- test/SL_2/num_bivector.py
+++ test/SL_2/num_bivector.py
@@ -17,7 +17,7 @@ for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qme
         mesh = np.load(f)
     for k in range(25):
         A = datetime.datetime.now()
-        npg.num_bivector(P_so3, mesh, pt_output=True)
+        npg.num_bivector(P_sl2, mesh, pt_output=True)
         B = datetime.datetime.now()
         tiempos[k] = (B - A).total_seconds()
     promedio = stat.mean(tiempos.values())
