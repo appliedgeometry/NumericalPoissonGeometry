0e209e68263abeb207d4dc2426506464a3abb7f8
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-09-14 10:16:38 -0500

    update num_modular_vf test

--- test/num_modular_vf.py
+++ test/num_modular_vf.py
@@ -10,7 +10,7 @@ P = {(1, 2): '1/4*x3*(x1**4 + x2**4 + x3**4)', (1, 3): '-1/4*x2*(x1**4 + x2**4 +
 
 num_modular_vf_res = dict()
 j = 2
-for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
+for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy', '3Qmesh_10_8.npy']:
     print(f'step {j}')
     tiempos = dict()
     with open(mesh_path, 'rb') as f:
@@ -25,6 +25,7 @@ for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qme
     tiempos['promedios'] = promedio
     tiempos['desviacion'] = desviacion
     num_modular_vf_res[f'10**{j}'] = tiempos
+    print(tiempos)
     j = j + 1
 
 print(num_modular_vf_res)
