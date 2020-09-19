c3e0ca8a675d204554159caebc4b0247c96f753a
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-09-19 08:53:49 -0500

    update num_coboundary_operator sl2 test

--- test/SL_2/num_coboundary_operator.py
+++ test/SL_2/num_coboundary_operator.py
@@ -13,7 +13,7 @@ W = {(1,): 'x1 * x3 * exp(-1/(x1**2 + x2**2 - x3**2)**2) / (x1**2 + x2**2)',
 
 num_coboundary_operator_res = dict()
 j = 2
-for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy', '3Qmesh_10_8.npy']:
+for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
     print(f'step {j}')
     tiempos = dict()
     with open(mesh_path, 'rb') as f:
@@ -28,6 +28,7 @@ for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qme
     tiempos['promedios'] = promedio
     tiempos['desviacion'] = desviacion
     num_coboundary_operator_res[f'10**{j}'] = tiempos
+    print(tiempos)
     j = j + 1
 
 print(num_coboundary_operator_res)
