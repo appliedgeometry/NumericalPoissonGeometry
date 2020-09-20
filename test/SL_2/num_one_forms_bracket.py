44676d2d10dedd1e51336a8e2ed587d3389b5949
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-09-20 01:44:01 -0500

    Update num_one_forms_bracket.py

--- test/SL_2/num_one_forms_bracket.py
+++ test/SL_2/num_one_forms_bracket.py
@@ -11,7 +11,7 @@ beta = {(1,): '1', (2,): '1', (3,): '1'}
 
 num_one_forms_bracket_res = dict()
 j = 2
-for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy' , '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy', , '3Qmesh_10_8.npy']:
+for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy' , '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
     print(f'step {j}')
     tiempos = dict()
     with open(mesh_path, 'rb') as f:
