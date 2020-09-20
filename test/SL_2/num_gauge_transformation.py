bff3537025ccd8050326c619f2557e8c2cdd2b0e
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-09-20 02:59:16 -0500

    Update num_gauge_transformation.py

--- test/SL_2/num_gauge_transformation.py
+++ test/SL_2/num_gauge_transformation.py
@@ -11,7 +11,7 @@ Lambda = {(1, 2): 'x2 - x1', (1, 3): 'x3 - x1', (2, 3): 'x2 - x3'}
 
 num_gauge_transformation_res = dict()
 j = 2
-for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy', '3Qmesh_10_8.npy']:
+for mesh_path in ['3Qmesh_10_2.npy', '3Qmesh_10_3.npy', '3Qmesh_10_4.npy', '3Qmesh_10_5.npy', '3Qmesh_10_6.npy', '3Qmesh_10_7.npy']:
     print(f'step {j}')
     tiempos = dict()
     with open(mesh_path, 'rb') as f:
