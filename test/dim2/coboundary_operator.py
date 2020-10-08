9daa122db0fb78c74744d09f249b117b3f63a0d8
Miguel Evangelista <miguel.eva.alv@gmail.com>
2020-10-08 11:00:45 -0500

    add the coboundary_operator test in dim 2

---
+++ test/dim2/coboundary_operator.py
@@ -0,0 +1,33 @@
+import datetime
+import time
+import numpy as np
+import statistics as stat
+from numpoisson.numpoisson import NumPoissonGeometry
+
+print('Start')
+npg = NumPoissonGeometry(2, 'x')
+P = {(1, 2): '(x1/2 + 1/4)'}
+W = {(1,2): '1'}
+
+num_coboundary_operator_res = dict()
+j = 2
+for mesh_path in ['2Qmesh_10_2.npy', '2Qmesh_10_3.npy', '2Qmesh_10_4.npy', '2Qmesh_10_5.npy', '2Qmesh_10_6.npy', '2Qmesh_10_7.npy']:
+    print(f'step {j}')
+    tiempos = dict()
+    with open(mesh_path, 'rb') as f:
+        mesh = np.load(f)
+    for k in range(25):
+        A = datetime.datetime.now()
+        npg.num_coboundary_operator(P, W, mesh, pt_output=True)
+        B = datetime.datetime.now()
+        tiempos[k] = (B - A).total_seconds()
+    promedio = stat.mean(tiempos.values())
+    desviacion = stat.pstdev(tiempos.values())
+    tiempos['promedios'] = promedio
+    tiempos['desviacion'] = desviacion
+    num_coboundary_operator_res[f'10**{j}'] = tiempos
+    print(tiempos)
+    j = j + 1
+
+print(num_coboundary_operator_res)
+print('Finish')
